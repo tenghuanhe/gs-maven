d8ffe03a3481d9de259168c401d837726f2fefc2
tenghuanhe <tenghuanhe@gmail.com>
2022-03-17 21:56:26 +0800

    fix test

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -1,17 +1,18 @@
 package hello;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-public class GreeterTest {
-	
-	private Greeter greeter = new Greeter();
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
 
-	@Test
-	public void greeterSaysHello() {
-		assertThat(greeter.sayHello(), containsString("Hello ci"));
-	}
+public class GreeterTest
+{
 
+  private final Greeter greeter = new Greeter();
+
+  @Test
+  public void greeterSaysHello()
+  {
+    assertThat(greeter.sayHello(), containsString("Hello"));
+  }
 }
